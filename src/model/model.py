e605cc3e38728e554d8ca840d4b94adfa9abc14d
Nitin Kumar <nk94.nitinkumar@gmail.com>
2018-12-09 13:13:42 -0500

    Created structure of project

---
+++ src/model/model.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+
+class GAN:
+    def __init__(self, num_features):
+        self.num_features = num_features
+        self.input_shape = [None, num_features, None]  # [batch_size, num_features, num_frames]
+
+    def build_model(self):
+        pass
+
+    def save_model(self):
+        pass
+
+    def load_model(self):
+        pass
+
+    def train_model(self):
+        pass
+
+    def evaluate_model(self):
+        pass
